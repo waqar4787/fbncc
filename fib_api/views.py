d653f47edcda6fdced3dc5edb4e08a019ec8e775
shivi74 <shivani.9487@gmail.com>
2016-09-07 10:12:35 +0530

    Django Project for nth number in sequence developed with templates

---
+++ fib_api/views.py
@@ -0,0 +1,43 @@
+import time
+
+from django.shortcuts import render
+from django.template import RequestContext
+from django.http import HttpResponse, HttpResponseRedirect
+
+from fib_api.models import FibonacciResults
+
+
+def fibonacci_calculation(num):
+    if num < 2:
+        result = num
+    else:
+        result = fibonacci_calculation(
+            num - 1) + fibonacci_calculation(num - 2)
+    return result
+
+
+def fib_number(request):
+    number = 0
+    result = 0
+    time_taken = 0
+
+    if request.GET.get('number'):
+        start_time = time.clock()
+        number = request.GET.get('number')
+        num = int(number)
+        result = fibonacci_calculation(num)
+        time_taken = time.clock() - start_time
+
+        obj = FibonacciResults.objects.create(
+            number=num, result=result, time_taken=time_taken)
+        obj.save()
+
+    return render(
+        request,
+        'index.html',
+        {
+            'number': num,
+            'result': result,
+            'time_taken': time_taken
+        }
+    )
